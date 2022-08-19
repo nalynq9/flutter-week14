02e07fba1a25f8f0e741d3939f3ec99d7e75fa06
nalynq9 <janisdinosaur@gmail.com>
2022-08-19 16:20:28 +0700

    up

--- lib/app.dart
+++ lib/app.dart
@@ -1,20 +1,16 @@
 import 'package:flutter/material.dart';
 
-import 'contact.dart';
-import 'home.dart';
-import 'list_view_demo.dart';
-import 'list_view_builder.dart';
-import 'list_view_menu.dart';
-import 'list_view_seperate.dart';
+import 'list_tile_view.dart';
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key); //1.constructor
 
   @override
-  Widget build(BuildContext buildContext) {
-    //2.build
+  Widget build(BuildContext context) {
     return MaterialApp(
-      home: ListViewMenu(),
+      home: ListTilePage(
+
+      ),
     );
   }
 }
